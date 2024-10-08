45e4242a13eeea78daae58fe7cf4ffbbc6306805
Maarij-Aqeel <maarijaqeel3200@gmail.com>
2024-10-08 21:41:29 +0500

    Automation using bash script

--- Automate.sh
+++ Automate.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
+#Sleep 
+
+#sleep 60
+
+#Virtual Environment
+
+#source /path/to/virtual/environment/
+
+#Navigate to folder
+
+cd /path/to/folder/
+
+#Adding Timestamp
+echo "Script started at $(date)" >> /path/to/log/file.txt
+#Run Script
+python3 main.py /path/to/folder/   >>  /path/to/log/file.txt 2>&1
+
+#Ending 
+echo "Script ended at $(date)" >> /path/to/log/file.txt
+
+echo "--------------------------------------------------------------------------------------" >>  /path/to/log/file.txt
+
+#Example of Automatically running on reboot in linux
+@reboot /path/to/bash/file.sh >> /path/to/log/file.txt 2>&1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
 # Uncomment the following line if you are using a virtual environment
 # source /path/to/your/venv/bin/activate
 
