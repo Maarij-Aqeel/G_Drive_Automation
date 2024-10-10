2a120a7cdb685ad13d707544b80f23e0bd03ac2c
Maarij-Aqeel <maarijaqeel3200@gmail.com>
2024-10-10 14:00:25 +0500

    Automation using bash script in linux

--- Automate.sh
+++ Automate.sh
@@ -40,16 +40,3 @@ echo "--------------------------------------------------------------------------
 
 
 
-#!/bin/bash
-
-# Uncomment the following line if you are using a virtual environment
-# source /path/to/your/venv/bin/activate
-
-# Navigate to the directory where your Python script is located
-cd /path/to/your/project
-
-# Run the Python script with any necessary arguments
-python3 main.py /path/to/your/folder -noreplace
-
-# Optionally, redirect output to a log file if needed
-# python3 main.py -storage >> /path/to/backup_log.txt 2>&1
